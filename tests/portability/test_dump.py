dffd8c66579c9c10e8f93c679f5fdc27e37c4917
Bankde <bankde@hotmail.com>
2023-10-06 01:19:42 +0700

    save

--- tests/portability/test_dump.py
+++ tests/portability/test_dump.py
@@ -270,6 +270,494 @@ def closure(y):
         return [x + z for z in y]
     return foo
 
+##### Specific test on OP #####
+
+def rotTwoThree():
+    # ROT_TWO and ROT_THREE.
+    a,b,c = 1,2,3
+    c,b,a = a,b,c
+    return a,b,c
+
+def dup():
+    # DUP_TOP
+    a,b,c = 1,2,3
+    while (a < b < c):
+        b += 1
+    return a
+
+def dupTopTwo():
+    # DUP_TOP_TWO
+    a = [1,[2,3]]
+    a[1] += [4]
+
+def bin_matmul():
+    # BINARY_MATRIX_MULTIPLY
+    pass
+
+def inplace_matmul():
+    # INPLACE_MATRIX_MULTIPLY
+    pass
+
+def bin_pow(x,y):
+    # BINARY_POWER
+    a = bin(x)
+    b = bin(y)
+    return a**b
+
+def bin_mul(x,y):
+    # BINARY_MULTIPLY
+    a = bin(x)
+    b = bin(y)
+    return a*b
+
+def bin_mod(x,y):
+    # BINARY_MODULO
+    a = bin(x)
+    b = bin(y)
+    return a % b
+
+def bin_add(x,y):
+    # BINARY_ADD
+    a = bin(x)
+    b = bin(y)
+    return a + b
+
+def bin_sub(x,y):
+    # BINARY_SUBTRACT
+    a = bin(x)
+    b = bin(y)
+    return a - b
+
+def bin_floordivide(x,y):
+    # BINARY_FLOOR_DIVIDE
+    a = bin(x)
+    b = bin(y)
+    return a // b
+
+def bin_truedivide(x,y):
+    # BINARY_TRUE_DIVIDE
+    a = bin(x)
+    b = bin(y)
+    return a / b
+
+def inplace_floor_divide(y):
+    # INPLACE_FLOOR_DIVIDE
+    x = [17]
+    x[0] //= y
+    return x[0]
+
+def inplace_true_divide(y):
+    # INPLACE_TRUE_DIVIDE
+    x = [17]
+    x[0] /= y
+
+def getlen():
+    # GET_LEN
+    pass
+
+def match_mapping():
+    # MATCH_MAPPING
+    pass
+
+def match_sequence():
+    # MATCH_SEQUENCE
+    pass
+
+def match_keys():
+    # MATCH_KEYS
+    pass
+
+def copy_dict_without_keys():
+    # COPY_DICT_WITHOUT_KEYS
+    pass
+
+def push_exc_info():
+    # PUSH_EXC_INFO
+    pass
+
+def check_exc_match():
+    # CHECK_EXC_MATCH
+    pass
+
+def check_eg_match():
+    # CHECK_EG_MATCH
+    pass
+
+def reraise():
+    # RERAISE
+    pass
+
+def with_except_start():
+    # WITH_EXCEPT_START
+    pass
+
+def begin_finally():
+    # BEGIN_FINALLY
+    pass
+
+def before_with():
+    # BEFORE_WITH
+    pass
+
+def end_async_for():
+    # END_ASYNC_FOR
+    pass
+
+def inplace_add():
+    # INPLACE_ADD
+    pass
+
+def inplace_sub():
+    # INPLACE_SUBTRACT
+    pass
+
+def inplace_mul():
+    # INPLACE_MULTIPLY
+    pass
+
+def inplace_mod():
+    # INPLACE_MODULO
+    pass
+
+def bin_lshift():
+    # BINARY_LSHIFT
+    pass
+
+def bin_rshift():
+    # BINARY_RSHIFT
+    pass
+
+def bin_and():
+    # BINARY_AND
+    pass
+
+def bin_xor():
+    # BINARY_XOR
+    pass
+
+def bin_or():
+    # BINARY_OR
+    pass
+
+def inplace_pow():
+    # INPLACE_POWER
+    pass
+
+def yield_from():
+    # YIELD_FROM
+    pass
+
+def get_awaitable():
+    # GET_AWAITABLE
+    pass
+
+def load_assert_err():
+    # LOAD_ASSERTION_ERROR
+    pass
+
+def inplace_lshift(y):
+    # INPLACE_LSHIFT
+    x = [17]
+    x[0] <<= y
+    return x[0]
+
+def return_generator():
+    # RETURN_GENERATOR
+    pass
+
+def inplace_rshift(y):
+    # INPLACE_RSHIFT
+    x = [17]
+    x[0] >>= y
+    return x[0]
+
+def inplace_and(y):
+    # INPLACE_AND
+    x = [17]
+    x[0] &= y
+    return x[0]
+
+def inplace_xor(y):
+    # INPLACE_XOR
+    x = [17]
+    x[0] ^= y
+    return x[0]
+
+def inplace_or(y):
+    # INPLACE_OR
+    x = [17]
+    x[0] |= y
+    return x[0]
+
+def break_loop(x):
+    # BREAK_LOOP
+    while True:
+        if x:
+            break
+
+def with_cleanup_start():
+    # WITH_CLEANUP_START
+    from decimal import Decimal, localcontext
+    with localcontext() as ctx:
+        ctx.prec = 42
+        x = Decimal("1") / Decimal("42")
+    return x
+
+def with_cleanup_finish():
+    # WITH_CLEANUP_FINISH
+    from decimal import Decimal, localcontext
+    with localcontext() as ctx:
+        ctx.prec = 42
+        x = Decimal("1") / Decimal("42")
+    return x
+
+def list_to_tuple():
+    # LIST_TO_TUPLE
+    ll = [1,2,3]
+    return (*ll,)
+
+def pop_block():
+    # POP_BLOCK
+    from decimal import Decimal, localcontext
+    with localcontext() as ctx:
+        ctx.prec = 42
+        x = Decimal("1") / Decimal("42")
+    return x
+
+def async_gen_wrap():
+    # ASYNC_GEN_WRAP
+    pass
+
+def end_finally():
+    # END_FINALLY
+    from decimal import Decimal, localcontext
+    with localcontext() as ctx:
+        ctx.prec = 42
+        x = Decimal("1") / Decimal("42")
+    return x
+
+def prep_reraise_star():
+    # PREP_RERAISE_STAR
+    pass
+
+def rot_n():
+    # ROT_N
+    pass
+
+def swap():
+    # SWAP
+    pass
+
+def jump_abs():
+    # JUMP_ABSOLUTE
+    pass
+
+def pop_jump_if_else():
+    # POP_JUMP_IF_FALSE
+    pass
+
+def pop_jump_forward_if_else():
+    # POP_JUMP_FORWARD_IF_FALSE
+    pass
+
+def pop_jump_if_true():
+    # POP_JUMP_IF_TRUE
+    pass
+
+def pop_jump_forward_if_true():
+    # POP_JUMP_FORWARD_IF_TRUE
+    pass
+
+def is_op():
+    # IS_OP
+    pass
+
+def contain_op():
+    # CONTAINS_OP
+    pass
+
+def continue_loop():
+    # CONTINUE_LOOP
+    pass
+
+# RERAISE dup with OP_48
+
+def setup_loop():
+    # SETUP_LOOP
+    pass
+
+def copy():
+    # COPY
+    pass
+
+def setup_except():
+    # SETUP_EXCEPT
+    pass
+
+def jump_if_not_exc_match():
+    # JUMP_IF_NOT_EXC_MATCH
+    pass
+
+def setup_finally():
+    # SETUP_FINALLY
+    pass
+
+def bin_op():
+    # BINARY_OP
+    pass
+
+def send():
+    # SEND
+    pass
+
+def pop_jump_forward_if_not_none():
+    # POP_JUMP_FORWARD_IF_NOT_NONE
+    pass
+
+def pop_jump_forward_if_none():
+    # POP_JUMP_FORWARD_IF_NONE
+    pass
+
+def call_func():
+    # CALL_FUNCTION
+    pass
+
+# GET_AWAITABLE - already declared in OP_73
+
+def jump_backward_no_interrupt():
+    # JUMP_BACKWARD_NO_INTERRUPT
+    pass
+
+def load_closure():
+    # LOAD_CLOSURE
+    pass
+
+def make_cell():
+    # MAKE_CELL
+    pass
+
+# The OP_num shift by 1 number for 3.11
+
+def load_deref():
+    # LOAD_DEREF
+    pass
+
+def store_deref():
+    # STORE_DEREF
+    pass
+
+def delete_deref():
+    # DELETE_DEREF
+    pass
+
+def jump_backward():
+    # JUMP_BACKWARD
+    pass
+
+def call_func_kw():
+    # CALL_FUNCTION_KW
+    pass
+
+def setup_with():
+    # SETUP_WITH
+    pass
+
+def build_list_unpack():
+    # BUILD_LIST_UNPACK
+    pass
+
+def copy_free_vars():
+    # COPY_FREE_VARS
+    pass
+
+def build_map_unpack():
+    # BUILD_MAP_UNPACK
+    pass
+
+def build_map_unpack_with_call():
+    # BUILD_MAP_UNPACK_WITH_CALL
+    pass
+
+def resume():
+    # RESUME
+    pass
+
+def build_tuple_unpack():
+    # BUILD_TUPLE_UNPACK
+    pass
+
+def match_class():
+    # MATCH_CLASS
+    pass
+
+def build_set_unpack():
+    # BUILD_SET_UNPACK
+    pass
+
+def setup_async_with():
+    # SETUP_ASYNC_WITH
+    pass
+
+def build_tuple_unpack_with_call():
+    # BUILD_TUPLE_UNPACK_WITH_CALL
+    pass
+
+def call_method():
+    # CALL_METHOD
+    pass
+
+def call_finally():
+    # CALL_FINALLY
+    pass
+
+def list_extend():
+    # LIST_EXTEND
+    pass
+
+def pop_finally():
+    # POP_FINALLY
+    pass
+
+def set_update():
+    # SET_UPDATE
+    pass
+
+def dict_merge():
+    # DICT_MERGE
+    pass
+
+def dict_update():
+    # DICT_UPDATE
+    pass
+
+def precall():
+    # PRECALL
+    pass
+
+def call():
+    # CALL
+    pass
+
+def kw_names():
+    # KW_NAMES
+    pass
+
+def pop_jump_backward_if_not_none():
+    # POP_JUMP_BACKWARD_IF_NOT_NONE
+    pass
+
+def pop_jump_backward_if_none():
+    # POP_JUMP_BACKWARD_IF_NONE
+    pass
+
+def pop_jump_backward_if_false():
+    # POP_JUMP_BACKWARD_IF_FALSE
+    pass
+
+def pop_jump_backward_if_true():
+    # POP_JUMP_BACKWARD_IF_TRUE
+    pass
+
 class Test(helper.PickleTestDump):
 
     def test_arith_ops(self):
@@ -360,6 +848,184 @@ class Test(helper.PickleTestDump):
         self.obj["ftf"] = self.dumps(try_finally)
         self.obj["ftfc"] = self.dumps(try_finallyconst)
         self.obj["ftfr"] = self.dumps(try_finallyreal)
+
+    def test_closure(self):
+        self.obj["f"] = self.dumps(closure)
+        c = closure(10)
+        self.obj["c"] = self.dumps(c)
+
+    def test_op_rot(self):
+        self.obj["rot"] = self.dumps(rotTwoThree)
+        self.obj["rotN"] = self.dumps(rot_n)
+        self.obj["swap"] = self.dumps(swap)
+
+    def test_op_dup(self):
+        self.obj["dupTop"] = self.dumps(dup)
+        self.obj["dupTopTwo"] = self.dumps(dupTopTwo)
+
+    def test_op_bin(self):
+        self.obj["binmatmul"] = self.dumps(bin_matmul)
+        self.obj["binpow"] = self.dumps(bin_pow)
+        self.obj["binmul"] = self.dumps(bin_mul)
+        self.obj["binpow"] = self.dumps(bin_pow)
+        self.obj["binmul"] = self.dumps(bin_mul)
+        self.obj["binmod"] = self.dumps(bin_mod)
+        self.obj["binadd"] = self.dumps(bin_add)
+        self.obj["binsub"] = self.dumps(bin_sub)
+        self.obj["binfloordivide"] = self.dumps(bin_floordivide)
+        self.obj["bintruedivide"] = self.dumps(bin_truedivide)
+        self.obj["binlshift"] = self.dumps(bin_lshift)
+        self.obj["binrfshift"] = self.dumps(bin_rshift)
+        self.obj["binand"] = self.dumps(bin_and)
+        self.obj["binxor"] = self.dumps(bin_xor)
+        self.obj["binor"] = self.dumps(bin_or)
+
+    def test_op_inplace(self):
+        self.obj["inplace_matmul"] = self.dumps(inplace_matmul)
+        self.obj["inplace_floor_divide"] = self.dumps(inplace_floor_divide)
+        self.obj["inplace_true_divide"] = self.dumps(inplace_true_divide)
+        self.obj["inplace_add"] = self.dumps(inplace_add)
+        self.obj["inplace_sub"] = self.dumps(inplace_sub)
+        self.obj["inplace_mul"] = self.dumps(inplace_mul)
+        self.obj["inplace_mod"] = self.dumps(inplace_mod)
+        self.obj["inplace_pow"] = self.dumps(inplace_pow)
+        self.obj["inplace_lshift"] = self.dumps(inplace_lshift)
+        self.obj["inplace_rshift"] = self.dumps(inplace_rshift)
+        self.obj["inplace_and"] = self.dumps(inplace_and)
+        self.obj["inplace_xor"] = self.dumps(inplace_xor)
+        self.obj["inplace_or"] = self.dumps(inplace_or)
+
+    def test_op_getlen(self):
+        self.obj["f"] = self.dumps(getlen)
+
+    def test_op_match(self):
+        self.obj["match_mapping"] = self.dumps(match_mapping)
+        self.obj["match_seq"] = self.dumps(match_sequence)
+        self.obj["match_keys"] = self.dumps(match_keys)
+        self.obj["match_class"] = self.dumps(match_class)
+
+    def test_op_copy_dict_no_keys(self):
+        self.obj["f"] = self.dumps(copy_dict_without_keys)
+
+    def test_op_exc(self):
+        self.obj["push_exc"] = self.dumps(push_exc_info)
+        self.obj["check_exc"] = self.dumps(check_exc_match)
+
+    def test_op_eg_match(self):
+        self.obj["check_eg_match"] = self.dumps(check_eg_match)
+
+    def test_op_context_except(self):
+        self.obj["reraise"] = self.dumps(reraise)
+        self.obj["with_except_start"] = self.dumps(with_except_start)
+        self.obj["prep_reraise_star"] = self.dumps(prep_reraise_star)
+        self.obj["setup_except"] = self.dumps(setup_except)
+
+    def test_op_context_async(self):
+        self.obj["end_async_for"] = self.dumps(end_async_for)
+        self.obj["setup_async_with"] = self.dumps(setup_async_with)
+
+    def test_op_context(self):
+        self.obj["before_with"] = self.dumps(before_with)
+        self.obj["begin_finally"] = self.dumps(begin_finally)
+        self.obj["end_finally"] = self.dumps(end_finally)
+        self.obj["setup_finally"] = self.dumps(setup_finally)
+        self.obj["setup_with"] = self.dumps(setup_with)
+
+    def test_op_async(self):
+        self.obj["yield_from"] = self.dumps(yield_from)
+        self.obj["get_awaitable"] = self.dumps(get_awaitable)
+    
+    def test_op_assert(self):
+        self.obj["load_assert_err"] = self.dumps(load_assert_err)
+
+    def test_op_return_gen(self):
+        self.obj["return_generator"] = self.dumps(return_generator)
+
+    def test_op_loop(self):
+        self.obj["break_loop"] = self.dumps(break_loop)
+        self.obj["continue_loop"] = self.dumps(continue_loop)
+        self.obj["setup_loop"] = self.dumps(setup_loop)
+
+    def test_op_cleanup(self):
+        self.obj["with_cleanup_start"] = self.dumps(with_cleanup_start)
+        self.obj["with_cleanup_finish"] = self.dumps(with_cleanup_finish)
+
+    def test_op_list(self):
+        self.obj["list_to_tuple"] = self.dumps(list_to_tuple)
+        self.obj["list_extend"] = self.dumps(list_extend)
+
+    def test_op_pop_block(self):
+        self.obj["f"] = self.dumps(pop_block)
+
+    def test_op_async_gen_wrap(self):
+        self.obj["f"] = self.dumps(async_gen_wrap)
+
+    def test_op_jump(self):
+        self.obj["jump_abs"] = self.dumps(jump_abs)
+        self.obj["pop_jump_if_else"] = self.dumps(pop_jump_if_else)
+        self.obj["pop_jump_forward_if_else"] = self.dumps(pop_jump_forward_if_else)
+        self.obj["pop_jump_if_true"] = self.dumps(pop_jump_if_true)
+        self.obj["pop_jump_forward_if_true"] = self.dumps(pop_jump_forward_if_true)
+        self.obj["jump_if_not_exc_match"] = self.dumps(jump_if_not_exc_match)
+        self.obj["pop_jump_forward_if_not_none"] = self.dumps(pop_jump_forward_if_not_none)
+        self.obj["pop_jump_forward_if_none"] = self.dumps(pop_jump_forward_if_none)
+        self.obj["jump_backward_no_interrupt"] = self.dumps(jump_backward_no_interrupt)
+        self.obj["jump_backward"] = self.dumps(jump_backward)
+        self.obj["pop_jump_backward_if_not_none"] = self.dumps(pop_jump_backward_if_not_none)
+        self.obj["pop_jump_backward_if_none"] = self.dumps(pop_jump_backward_if_none)
+        self.obj["pop_jump_backward_if_false"] = self.dumps(pop_jump_backward_if_false)
+        self.obj["pop_jump_backward_if_true"] = self.dumps(pop_jump_backward_if_true)
+
+    def test_op_op(self):
+        self.obj["is_op"] = self.dumps(is_op)
+        self.obj["contain_op"] = self.dumps(contain_op)
+        self.obj["bin_op"] = self.dumps(bin_op)
+
+    def test_op_copy(self):
+        self.obj["f"] = self.dumps(copy)
+
+    def test_op_send(self):
+        self.obj["f"] = self.dumps(send)
+
+    def test_op_call(self):
+        self.obj["call_func"] = self.dumps(call_func)
+        self.obj["call_func_kw"] = self.dumps(call_func_kw)
+        self.obj["call_method"] = self.dumps(call_method)
+        self.obj["call_finally"] = self.dumps(call_finally)
+        self.obj["precall"] = self.dumps(precall)
+        self.obj["call"] = self.dumps(call)
+
+    def test_op_closure(self):
+        self.obj["load_closure"] = self.dumps(load_closure)
+        self.obj["make_cell"] = self.dumps(make_cell)
+        self.obj["load_deref"] = self.dumps(load_deref)
+        self.obj["store_deref"] = self.dumps(store_deref)
+        self.obj["delete_deref"] = self.dumps(delete_deref)
+        self.obj["copy_free_vars"] = self.dumps(copy_free_vars)
+        
+    def test_op_unpack(self):
+        self.obj["build_list_unpack"] = self.dumps(build_list_unpack)
+        self.obj["build_map_unpack"] = self.dumps(build_map_unpack)
+        self.obj["build_map_unpack_with_call"] = self.dumps(build_map_unpack_with_call)
+        self.obj["build_tuple_unpack"] = self.dumps(build_tuple_unpack)
+        self.obj["build_set_unpack"] = self.dumps(build_set_unpack)
+        self.obj["build_tuple_unpack_with_call"] = self.dumps(build_tuple_unpack_with_call)
+
+    def test_op_resume(self):
+        self.obj["resume"] = self.dumps(resume)
+
+    def test_op_pop_finally(self):
+        self.obj["pop_finally"] = self.dumps(pop_finally)
+
+    def test_op_set(self):
+        self.obj["set_update"] = self.dumps(set_update)
+
+    def test_op_dict(self):
+        self.obj["dict_merge"] = self.dumps(dict_merge)
+        self.obj["dict_update"] = self.dumps(dict_update)
+
+    def test_op_kw_names(self):
+        self.obj["kw_names"] = self.dumps(kw_names)
 
 if __name__ == "__main__":
     unittest.main()
